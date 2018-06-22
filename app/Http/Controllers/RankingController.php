f75b31f4360b43cf37a2b929aab2b6ca87d62bc9
rika.hoshino <ec2-user@ip-172-31-37-36.us-east-2.compute.internal>
2018-06-22 06:55:35 +0000

    resubmit

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -16,6 +16,7 @@ class RankingController extends Controller
 
         return view('ranking.want', [
             'items' => $items,
+            'unit' =>"want",
         ]);
     }
     
@@ -25,6 +26,7 @@ class RankingController extends Controller
 
         return view('ranking.have', [
             'items' => $items,
+            'unit' =>"have"
             ]);
     }
 }
