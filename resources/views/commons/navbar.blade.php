36643fcf1be1366b106e718c7c801e618a1f0c48
rika.hoshino <ec2-user@ip-172-31-37-36.us-east-2.compute.internal>
2018-06-22 05:44:45 +0000

    ranking

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,10 +14,21 @@
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::check())
                         <li>
-                            <a href="{{ route('items.create') }}">
+                            <a href="#">
                                 <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
                                 アイテムを追加
                               </a>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <span class="glyphicon glyphicon-signal" aria-hidden="true"></span>
+                                ランキング
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ route('ranking.want') }}">Wantランキング</a></li>
+                            </ul>
                         </li>
 
                         <li class="dropdown">
@@ -30,8 +41,7 @@
                             </a>
                             <ul class="dropdown-menu">
                                 <li>
-                                     <a href="{{ route('users.show', Auth::id()) }}">マイページ</a>
-                                    
+                                    <a href="{{ route('users.show', Auth::id()) }}">マイページ</a>
                                 </li>
                                 <li role="separator" class="divider"></li>
                                 <li>
@@ -40,6 +50,7 @@
                             </ul>
                         </li>
                     @else
+
                         <li><a href="{{ route('signup.get') }}">新規登録</a></li>
                         <li><a href="{{ route('login') }}">ログイン</a></li>
                     @endif
